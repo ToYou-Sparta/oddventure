151380cb22c5ead3402ccd96e4c6fae583a46d3d
nyong0313 <ghdyd6625@naver.com>
2025-10-14 19:06:14 +0900

    feat: 기본 구조 설정

---
+++ src/main/java/org/example/oddventure/common/exception/ErrorCode.java
@@ -0,0 +1,12 @@
+package org.example.oddventure.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCode {
+
+    String name();
+
+    HttpStatus getHttpStatus();
+
+    String getMessage();
+}
