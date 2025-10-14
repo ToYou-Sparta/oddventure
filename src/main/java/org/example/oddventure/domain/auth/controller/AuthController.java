cc762e3909da182e891adbf7bd93b6fdc674eb7f
LeeDongJae-KR <movementj98@gmail.com>
2025-10-14 20:03:43 +0900

    feat: auth controller 추가

---
+++ src/main/java/org/example/oddventure/domain/auth/controller/AuthController.java
@@ -0,0 +1,45 @@
+package org.example.oddventure.domain.auth.controller;
+
+import org.example.oddventure.common.dto.reponse.ApiResponse;
+import org.example.oddventure.domain.auth.dto.request.LoginRequest;
+import org.example.oddventure.domain.auth.dto.request.SignUpRequest;
+import org.example.oddventure.domain.auth.dto.request.WithdrawRequest;
+import org.example.oddventure.domain.auth.dto.response.LoginResponse;
+import org.example.oddventure.domain.auth.dto.response.SignupResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponse<SignupResponse>> signup(
+            @RequestBody SignUpRequest signUpRequest) {
+
+        return null;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<LoginResponse>> login(
+            @RequestBody LoginRequest loginRequest) {
+
+        return null;
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<ApiResponse<Object>> logout(
+            @RequestHeader("Authorization") String token,
+            @RequestBody WithdrawRequest withdrawRequest) {
+
+        return null;
+    }
+
+    @PostMapping("/withdraw")
+    public ResponseEntity<ApiResponse<Object>> withdraw(
+            @RequestHeader("Authorization") String token,
+            @RequestBody WithdrawRequest withdrawRequest) {
+
+        return null;
+    }
+}
