40a2375fdf288a60b925b08c7519db2bdb32203d
Main <twodc@naver.com>
2025-10-14 18:07:43 +0900

    first commit

---
+++ src/test/java/org/example/oddventure/OddventureApplicationTests.java
@@ -0,0 +1,14 @@
+package org.example.oddventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+class OddventureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
