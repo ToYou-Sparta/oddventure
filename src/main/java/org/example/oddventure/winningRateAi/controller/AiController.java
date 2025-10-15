1ba0e3ff6c545355928ae5d36fcb422cd82319ec
eunseo04 <jes040616@naver.com>
2025-10-15 20:37:35 +0900

    feat: spring ai 적용

---
+++ src/main/java/org/example/oddventure/winningRateAi/controller/AiController.java
@@ -0,0 +1,25 @@
+package org.example.oddventure.winningRateAi.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AiController {
+
+    private final ChatClient chatClient;
+
+    public AiController(ChatClient.Builder chatClient) {
+        this.chatClient = chatClient.build();
+    }
+
+    @GetMapping("/winningrate")
+    String generation(@RequestBody String userInput){
+        return this.chatClient.prompt()
+                .system("한국어로 대답하고 해당 팀의 전적과 계산한 승률에 대한 이유를 나열해.")
+                .user(userInput)
+                .call()
+                .content();
+    }
+}
