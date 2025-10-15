54778a193601d52cf23b04377af1c9cb685ecbeb
LeeDongJae-KR <movementj98@gmail.com>
2025-10-15 18:54:37 +0900

    feat: 인증된 사용자 정보를 담는 AuthUser DTO 추가

---
+++ src/main/java/org/example/oddventure/domain/common/dto/AuthUser.java
@@ -0,0 +1,30 @@
+package org.example.oddventure.domain.common.dto;
+
+import lombok.Getter;
+import org.example.oddventure.domain.user.enums.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+public class AuthUser {
+
+    private final Long id;
+    private final String username;
+    private final String email;
+    private final UserRole userRole;
+
+    public AuthUser(Long id, String username, String email, UserRole userRole) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.userRole = userRole;
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(userRole.getUserRole()));
+    }
+
+}
