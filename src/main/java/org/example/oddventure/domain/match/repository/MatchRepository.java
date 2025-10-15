b9e518ba12d51af3b0e0032c685630d113aadf87
Park-yh <pyh5421@naver.com>
2025-10-15 17:36:15 +0900

    refactor: MatchRepository findByConditions 주석 처리

--- src/main/java/org/example/oddventure/domain/match/repository/MatchRepository.java
+++ src/main/java/org/example/oddventure/domain/match/repository/MatchRepository.java
@@ -1,13 +1,10 @@
 package org.example.oddventure.domain.match.repository;
 
 import org.example.oddventure.domain.match.entity.Match;
-import org.example.oddventure.domain.match.enums.MatchStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface MatchRepository extends JpaRepository<Match, Long> {
 
     // TODO: JPQL로 구현
-    List<Match> findByConditions(String teamName, MatchStatus status);
+//    List<Match> findByConditions(String teamName, MatchStatus status);
 }
