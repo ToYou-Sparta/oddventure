9e70af16379939433cd67876718cca854206fd77
LeeDongJae-KR <movementj98@gmail.com>
2025-10-15 17:44:24 +0900

    feat: UserRole 필드 추가

--- src/main/java/org/example/oddventure/domain/user/entity/User.java
+++ src/main/java/org/example/oddventure/domain/user/entity/User.java
@@ -1,14 +1,11 @@
 package org.example.oddventure.domain.user.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.oddventure.domain.user.enums.UserRole;
 
 @Entity
 @Getter
@@ -27,10 +24,15 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole userRole;
+
     @Builder
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password,  UserRole userRole) {
         this.username = username;
         this.email = email;
         this.password = password;
+        this.userRole = userRole;
     }
 }
