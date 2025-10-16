15e8a6eabf77811dd0a8398b3de2259cf8a35f5f
eunseo04 <jes040616@naver.com>
2025-10-17 02:11:40 +0900

    feat: 인기 검색어 api 구현

---
+++ src/main/java/org/example/oddventure/domain/hotKeywords/controller/HotKeywordsController.java
@@ -0,0 +1,20 @@
+package org.example.oddventure.domain.hotKeywords.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.oddventure.common.dto.response.ApiResponse;
+import org.example.oddventure.domain.hotKeywords.dto.HotKeywordsResponse;
+import org.example.oddventure.domain.hotKeywords.service.HotKeywordsService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class HotKeywordsController {
+    private final HotKeywordsService hotKeywordsService;
+
+    @GetMapping("/api/v1/hotkeyword")
+    public ResponseEntity<ApiResponse<HotKeywordsResponse>> getHotKeywords() {
+        return ApiResponse.success(hotKeywordsService.getHotKeywords());
+    }
+}
