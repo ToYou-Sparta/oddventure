d06becef689df64a49fd245a15bf0376a90bb890
LeeDongJae-KR <movementj98@gmail.com>
2025-10-16 11:47:09 +0900

    refactor: 에러 코드 부분 삭제

--- src/main/java/org/example/oddventure/domain/common/exception/AuthErrorCode.java
+++ src/main/java/org/example/oddventure/domain/common/exception/AuthErrorCode.java
@@ -7,10 +7,10 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
-public enum ServerErrorCode implements ErrorCode {
-    JWT_CANNOT_FIND_TOKEN("SER-001", HttpStatus.NOT_FOUND, "JWT 토큰을 찾을 수 없습니다.");
+public enum AuthErrorCode implements ErrorCode {
+    JWT_CANNOT_FIND_TOKEN(HttpStatus.NOT_FOUND, "JWT 토큰을 찾을 수 없습니다.");
 
-    private final String code;
+    // private final String code;
     private final HttpStatus httpStatus;
     private final String message;
 }
--- src/main/java/org/example/oddventure/domain/user/exception/UserErrorCode.java
+++ src/main/java/org/example/oddventure/domain/user/exception/UserErrorCode.java
@@ -8,12 +8,12 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum UserErrorCode implements ErrorCode {
-    USR_INVALID_USER_ID("USR-001", HttpStatus.BAD_REQUEST, "유효하지 않은 유저 ID 입니다."),
-    USR_INVALID_USER_ROLE("USR-002", HttpStatus.BAD_REQUEST, "유효하지 않은 유저 권한"),
-    USR_PASSWORD_INCORRECT("USR-003", HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다.");
+    USR_INVALID_USER_ID( HttpStatus.BAD_REQUEST, "유효하지 않은 유저 ID 입니다."),
+    USR_INVALID_USER_ROLE(HttpStatus.BAD_REQUEST, "유효하지 않은 유저 권한"),
+    USR_PASSWORD_INCORRECT(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다.");
 
 
-    private final String code;
+    //private final String code;
     private final HttpStatus httpStatus;
     private final String message;
 }
