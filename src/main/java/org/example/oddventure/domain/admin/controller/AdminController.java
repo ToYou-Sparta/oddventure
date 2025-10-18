df0abf0708a2ec6473b269b36b3babcfcd171fc5
Park-yh <pyh5421@naver.com>
2025-10-19 08:55:57 +0900

    refactor: 오타 수정 및 restcontroller 변경

--- src/main/java/org/example/oddventure/domain/admin/controller/AdminController.java
+++ src/main/java/org/example/oddventure/domain/admin/controller/AdminController.java
@@ -12,7 +12,6 @@ import org.example.oddventure.domain.admin.service.AdminService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,8 +19,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/admin")
 public class AdminController {
