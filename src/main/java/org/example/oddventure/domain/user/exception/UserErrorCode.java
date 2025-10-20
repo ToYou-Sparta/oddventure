b71c9094cee72f287c00df2d86aa4f66b91a8e3c
Park-yh <pyh5421@naver.com>
2025-10-20 14:06:17 +0900

    refactor: 오타 수정

--- src/main/java/org/example/oddventure/domain/user/exception/UserErrorCode.java
+++ src/main/java/org/example/oddventure/domain/user/exception/UserErrorCode.java
@@ -11,7 +11,7 @@ public enum UserErrorCode implements ErrorCode {
     USR_INVALID_USER_ID(HttpStatus.BAD_REQUEST, "유효하지 않은 유저 ID 입니다."),
     USR_INVALID_USER_ROLE(HttpStatus.BAD_REQUEST, "유효하지 않은 유저 권한"),
     USR_PASSWORD_INCORRECT(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다."),
-    ALREADY_EXIST_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다.");
+    ALREADY_EXIST_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 이메일 입니다.");
 
     //private final String code;
     private final HttpStatus httpStatus;
