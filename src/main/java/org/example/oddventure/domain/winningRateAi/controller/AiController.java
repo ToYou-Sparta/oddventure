d51976dc9440aa00857fe9e007c6540858c56438
eunseo04 <jes040616@naver.com>
2025-10-21 20:56:20 +0900

    refactor: 공통 응답으로 수정

--- src/main/java/org/example/oddventure/domain/winningRateAi/controller/AiController.java
+++ src/main/java/org/example/oddventure/domain/winningRateAi/controller/AiController.java
@@ -1,11 +1,12 @@
 package org.example.oddventure.domain.winningRateAi.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.oddventure.common.dto.response.ApiResponse;
+import org.example.oddventure.domain.winningRateAi.dto.AiRequest;
+import org.example.oddventure.domain.winningRateAi.dto.AiResponse;
 import org.example.oddventure.domain.winningRateAi.service.AiService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +16,8 @@ public class AiController {
     private final AiService aiService;
 
     @GetMapping("/winningrate")
-    String generation(@RequestBody String userInput){
-        return aiService.generateAbnormalBehaviorReport(userInput);
+    ResponseEntity<ApiResponse<AiResponse>> generation(@RequestBody String content) {
+        AiRequest request = new AiRequest(content);
+        return ApiResponse.success(aiService.generateAbnormalBehaviorReport(request), "ai 기반 승률 예측 결과를 반환한다.");
     }
 }
