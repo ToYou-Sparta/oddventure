64c6ef9e74d9d26885998900739b66627d747f33
eunseo04 <jes040616@naver.com>
2025-10-21 22:15:30 +0900

    refactor: response dto 수정

--- src/main/java/org/example/oddventure/domain/winningRateAi/dto/AiResponse.java
+++ src/main/java/org/example/oddventure/domain/winningRateAi/dto/AiResponse.java
@@ -2,13 +2,18 @@ package org.example.oddventure.domain.winningRateAi.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record AiResponse(
         boolean result,
         boolean hasTeamName,
-        String teamName,
-        Long winningCount,
-        Long losingCount,
-        String winningRate,
+        List<String> teamName,
+        List<Long> winningCount,
+        List<Long> losingCount,
+        List<Long> winningRate,
         String content
 ) {}
+
+//변수가 하나라 여러 팀의 승률을 반환하지 못하는 문제 발생
+//리스트로 반환
