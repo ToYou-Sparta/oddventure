ceeb2c0cb8f7317612941f62f47856bcab983ab1
eunseo04 <jes040616@naver.com>
2025-10-23 21:25:18 +0900

    refactor: 공통응답으로 변경 및 수정

--- src/test/java/org/example/oddventure/domain/hotkeyword/HotkeywordControllerTest.java
+++ src/test/java/org/example/oddventure/domain/hotkeyword/HotkeywordControllerTest.java
@@ -1,58 +1,37 @@
 package org.example.oddventure.domain.hotkeyword;
 
-import org.example.oddventure.base.RedisTestContainerConfig;
-import org.example.oddventure.base.WithMockAuthUser;
 import org.example.oddventure.domain.auth.config.SecurityConfig;
 import org.example.oddventure.domain.auth.jwt.JwtUtil;
 import org.example.oddventure.domain.hotKeywords.controller.HotKeywordsController;
 import org.example.oddventure.domain.hotKeywords.dto.HotKeywordsResponse;
 import org.example.oddventure.domain.hotKeywords.service.HotKeywordsService;
-import org.example.oddventure.domain.user.enums.UserRole;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationContextProvider;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
-import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.filter.CharacterEncodingFilter;
-import java.nio.charset.StandardCharsets;
+import org.springframework.test.web.servlet.ResultActions;
+import org.example.oddventure.base.restdocs.RestDocsTestSupport;
+import org.example.oddventure.base.restdocs.RestDocsUtils;
 import java.util.Set;
 import static org.mockito.Mockito.when;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(HotKeywordsController.class)
 @Import({SecurityConfig.class, JwtUtil.class})
-@WithMockAuthUser(userId = 1, role = UserRole.ROLE_USER)
-@ExtendWith(RestDocumentationExtension.class)
-public class HotkeywordControllerTest extends RedisTestContainerConfig {
-
-    private RestDocumentationResultHandler restDocs;
-
-    private MockMvc mockMvc;
-
-    private Set<Object> top5;
+public class HotkeywordControllerTest extends RestDocsTestSupport{
 
     @MockitoBean
     private HotKeywordsService hotKeywordsService;
 
-    @BeforeEach
-    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
-        top5 = Set.of(
+    @Test
+    @DisplayName("인기 검색어 Top5를 조회할 수 있다.")
+    void getTop5HotKeywords_success() throws Exception {
+        // given
+        Set<Object> top5 = Set.of(
                 "korea",
                 "america",
                 "china",
@@ -61,31 +40,18 @@ public class HotkeywordControllerTest extends RedisTestContainerConfig {
                 "japan"
         );
 
-        this.restDocs = MockMvcRestDocumentation.document("{class-name}/{method-name}",
-                preprocessRequest(prettyPrint()),
-                preprocessResponse(prettyPrint()));
-
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .addFilter(new CharacterEncodingFilter(StandardCharsets.UTF_8.name(), true))
-                .apply(MockMvcRestDocumentation.documentationConfiguration(restDocumentation))
-                .apply(SecurityMockMvcConfigurers.springSecurity())
-                .alwaysDo(restDocs) // 디폴트 설정
-                .build();
-    }
-
-    @Test
-    @DisplayName("인기 검색어 Top5를 조회할 수 있다.")
-    void getTop5HotKeywords_success() throws Exception {
-        // given
         HotKeywordsResponse response = HotKeywordsResponse.of(top5);
         when(hotKeywordsService.getHotKeywords()).thenReturn(response);
 
         // when & then
-        mockMvc.perform(get("/api/v1/hotkeyword")
-                        .contentType(MediaType.APPLICATION_JSON))
+        ResultActions result = mockMvc.perform(get("/api/v1/hotkeyword")
+                        .contentType(MediaType.APPLICATION_JSON));
+
+
+        result.andExpect(status().isCreated())
                 .andExpect(status().isOk())
                 .andDo(restDocs.document(
-                        responseFields(
+                        RestDocsUtils.successWithDataFields(
                                 fieldWithPath("httpStatus").description("HTTP 상태 코드"),
                                 fieldWithPath("code").description("응답 코드"),
                                 fieldWithPath("success").description("성공 여부"),
