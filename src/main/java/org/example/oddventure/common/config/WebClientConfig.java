082665911de43904ca86fa1a0a832b7eeca8b83a
nyong0313 <ghdyd6625@naver.com>
2025-10-24 12:22:51 +0900

    feat: WebClient 설정

---
+++ src/main/java/org/example/oddventure/common/config/WebClientConfig.java
@@ -0,0 +1,36 @@
+package org.example.oddventure.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Value("${match-data.grid.central-data.base-url}")
+    private String GRID_CENTRAL_BASE_URL;
+
+    @Value("${match-data.grid.live-data.base-url}")
+    private String GRID_LIVE_BASE_URL;
+
+    @Value("${match-data.grid.api-key}")
+    private String GRID_API_KEY;
+
+    @Bean
+    public WebClient gridCentralClient() {
+        return WebClient.builder()
+                .baseUrl(GRID_CENTRAL_BASE_URL)
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + GRID_API_KEY)
+                .build();
+    }
+
+    @Bean
+    public WebClient gridLiveClient() {
+        return WebClient.builder()
+                .baseUrl(GRID_LIVE_BASE_URL)
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + GRID_API_KEY)
+                .build();
+    }
+}
