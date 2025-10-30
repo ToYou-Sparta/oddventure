4b087f5dc407b5ec382abdacc9a615f9ed15ed6a
nyong0313 <ghdyd6625@naver.com>
2025-10-30 15:50:41 +0900

    feat: 매치 결과 자동 업데이트

--- src/main/java/org/example/oddventure/domain/grid/dto/response/field/SeriesState.java
+++ src/main/java/org/example/oddventure/domain/grid/dto/response/field/SeriesState.java
@@ -3,6 +3,7 @@ package org.example.oddventure.domain.grid.dto.response.field;
 import java.util.List;
 
 public record SeriesState(
+        boolean finished,
         List<Team> teams
 ) {
     public record Team(
