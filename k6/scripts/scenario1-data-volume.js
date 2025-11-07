d349569e58dce1d17ffdc116a2759c46a4b218fe
LeeDongJae-KR <movementj98@gmail.com>
2025-11-07 17:42:03 +0900

    test: 모든 검색어 길이를 합쳐서 계산하도록 수정

--- k6/scripts/scenario1-data-volume.js
+++ k6/scripts/scenario1-data-volume.js
@@ -96,7 +96,6 @@ export default function () {
         },
         tags: {
             name: 'DataVolumeTest',
-            keyword_length: searchCondition.keyword.length.toString(),
         },
     };
 
