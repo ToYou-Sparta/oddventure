47e8c33b427e7cadf1e688cf62d1dd7c7eeac9aa
eunseo04 <jes040616@naver.com>
2025-11-10 16:30:49 +0900

    feat: controller를 통해 aiAgentRunner 실행

--- src/main/java/org/example/oddventure/domain/ai/controller/AiController.java
+++ src/main/java/org/example/oddventure/domain/ai/controller/AiController.java
@@ -1,11 +1,7 @@
 package org.example.oddventure.domain.ai.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.example.oddventure.common.dto.response.ApiResponse;
-import org.example.oddventure.domain.ai.dto.AiRequest;
-import org.example.oddventure.domain.ai.dto.AiResponse;
-import org.example.oddventure.domain.ai.service.AiService;
-import org.springframework.http.ResponseEntity;
+import org.example.oddventure.domain.ai.AgentRunnerService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,10 +9,10 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/ai")
 public class AiController {
 
-    private final AiService aiService;
+    private final AgentRunnerService agentRunnerService;
 
     @PostMapping("/winningrate")
-    ResponseEntity<ApiResponse<AiResponse>> generation(@RequestBody AiRequest request) {
-        return ApiResponse.success(aiService.calculateWinningRateWithAi(request), "ai 기반 승률 예측 결과를 반환한다.");
+    public Object generation(@RequestBody String request) throws Exception {
+        return agentRunnerService.execute(request);
     }
 }
