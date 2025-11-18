88e6f1357be8db0bdaff3f17a3e670029a9eb919
LeeDongJae-KR <movementj98@gmail.com>
2025-11-18 22:20:22 +0900

    feat: elasticsearch local 환경 설정 파일 작성

---
+++ src/main/java/org/example/oddventure/common/config/ElasticsearchLocalConfig.java
@@ -0,0 +1,36 @@
+package org.example.oddventure.common.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Profile({"local", "dev"})
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "org.example.oddventure.domain.match.repository.elasticsearch")
+public class ElasticsearchLocalConfig extends AbstractElasticsearchConfiguration {
+
+    @Value("${spring.elasticsearch.uris}")
+    private String elasticsearchUri;
+
+    @Override
+    @Bean
+    public RestHighLevelClient elasticsearchClient() {
+        String host = elasticsearchUri
+                .replace("https://", "")
+                .replace("http://", "")
+                .split(":")[0];
+
+        int port = elasticsearchUri.contains(":443") ? 443 : 9200;
+        String scheme = elasticsearchUri.startsWith("https") ? "https" : "http";
+
+        return new RestHighLevelClient(
+                RestClient.builder(new HttpHost(host, port, scheme))
+        );
+    }
+}
